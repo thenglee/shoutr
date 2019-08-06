f9094b8c4af356dcfc510c2a3ea4204af78ee47f
Tan Theng Lee <thengleetan@gmail.com>
2019-08-06 16:41:23 +0800

    Extract Timeline and Dashboard classes
    Using Single Responsbility Principle
    
     Please enter the commit message for your changes. Lines starting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,10 @@ class UsersController < Clearance::UsersController
   def new
     @user = User.new
   end
-  
+
   def show
     @user = User.find_by(username: params[:id])
-    @shouts = @user.shouts
+    @timeline = Timeline.new([@user])
   end
 
   private
